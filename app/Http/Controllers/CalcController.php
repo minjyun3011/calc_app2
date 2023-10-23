e5304fc75c0b93e96fc02937664a173d5b042c26
minjyun3011 <grand3canyon10@gmail.com>
2023-10-23 11:36:24 +0900

    計算アプリ課題完了

---
+++ app/Http/Controllers/CalcController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CalcController extends Controller
+{
+    public function result($num1, $operator, $num2)
+    {
+        $result = 0;
+        if ($operator == 'addition') {
+            $result = $num1 + $num2;
+        }
+        elseif ($operator == 'subtraction') {
+            $result = $num1 - $num2;
+        }
+        elseif ($operator == 'multiplication') {
+            $result = $num1 * $num2;
+        }
+        elseif ($operator == 'division') {
+            $result = $num1 / $num2;
+        }
+        else {
+            $result =  '正しく入力してください';
+        }
+        return view ('message.calc', ['result' => $result]);
+    }
+}
